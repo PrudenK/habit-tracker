a98852a1c910612427aec5103c5df1ef2a3112aa
Pruden <prudencosta@gmail.com>
2025-04-30 10:19:15 +0200

    Opciones toolbar

--- app/src/main/java/com/pruden/habits/modules/miniHabitos/MiniHabitosFragment.kt
+++ app/src/main/java/com/pruden/habits/modules/miniHabitos/MiniHabitosFragment.kt
@@ -3,8 +3,14 @@ package com.pruden.habits.modules.miniHabitos
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.pruden.habits.R
 import com.pruden.habits.databinding.FragmentMiniHabitosBinding
 
 class MiniHabitosFragment : Fragment() {
@@ -25,6 +31,50 @@ class MiniHabitosFragment : Fragment() {
 
 
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarFragmentMiniHabitos)
+
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+
+        val back = ContextCompat.getDrawable(requireContext(), R.drawable.ic_back)
+        back?.setTint(ContextCompat.getColor(requireContext(), R.color.lightGrayColor))
+        (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(back)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_tool_bar_mini_habitos, menu)
+        val item = menu.findItem(R.id.informacion)
+        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_interrogacion)
+
+        drawable?.setTint(ContextCompat.getColor(requireContext(), R.color.lightGrayColor))
+        item.icon = drawable
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            android.R.id.home -> {
+                activity?.onBackPressed()
+                true
+            }
+            R.id.desarchivar_todos ->{
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
