27c0c75e60be4c2de165b6428d3fea1ce19740b3
Pruden <prudencosta@gmail.com>
2025-07-02 05:34:19 +0200

    targetSdk = 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.pruden.habits"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0"
 
